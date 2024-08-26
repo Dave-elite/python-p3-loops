3687cc56fe2ccfa20590fdfa51a09d0e290f7e75
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-08-26 12:30:15 +0300

    lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,34 @@
 
 def happy_new_year():
     # code goes here!
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+    print('Happy new year!')
+        
+    
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num *num)
+    return squared_list
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+    
     pass
